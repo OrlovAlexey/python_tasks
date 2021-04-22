aca9060bccd4befa1ac8226a2c2f5f172331d9a8
Alex <orlov.aleksei@phystech.edu>
2021-04-23 00:47:00 +0300

    7th correction

--- review1_encryption.py
+++ review1_encryption.py
@@ -25,30 +25,29 @@ class AbstractCipher:
         pass
 
 class Caesar(AbstractCipher):
+    alphabet : str
     def __init__(self, language):
         super().__init__()
-        self.language = language
+        if language == "eng":
+            self.alphabet = alphabet_eng
+        elif language == "rus":
+            self.alphabet = alphabet_rus
 
     def encrypt(self, input_str):
         bias = int(input("Enter the bias, please: "))
-        return caesar(input_str, bias, self.language)
+        return caesar(input_str, bias)
 
     def decrypt(self, input_str):
         bias = int(input("Enter the bias, please: "))
-        return caesar_decryption(input_str, bias, self.language)
+        return caesar_decryption(input_str, bias)
 
-    def caesar(input_str, bias, language = "eng"):
+    def caesar(input_str, bias):
         """ Caesar encryption algorithm
             input_str is the string to encrypt
             bias is the key (shifts the alphabet bias times to the right)
             language can be "eng" or "rus"
         """
-        
-        if language == "eng":
-            alphabet = alphabet_eng
-        elif language == "rus":
-            alphabet = alphabet_rus
-        
+        alphabet = self.alphabet
         alphabet_length = len(alphabet)
         new_str = []
         for letter in input_str:
@@ -63,33 +62,33 @@ class Caesar(AbstractCipher):
         output_str = ''.join(new_str)
         return output_str
     
-    def caesar_decryption(input_str, bias, language = "eng"):
-        return caesar(input_str, -bias, language)
+    def caesar_decryption(input_str, bias):
+        return caesar(input_str, -bias)
 
 class Viginere(AbstractCipher):
+    alphabet : str
     def __init__(self, language):
         super().__init__()
-        self.language = language
+        if language == "eng":
+            self.alphabet = alphabet_eng
+        elif language == "rus":
+            self.alphabet = alphabet_rus
 
     def encrypt(self, input_str):
         keyword = str(input("Enter the keyword, please: "))
-        return vigenere(input_str, keyword, self.language)
+        return vigenere(input_str, keyword)
 
     def decrypt(self, input_str):
         keyword = str(input("Enter the keyword, please: "))
-        return vigenere_decryption(input_str, keyword, self.language)
+        return vigenere_decryption(input_str, keyword)
 
-    def vigenere(input_str, key_word, language = "eng"):
+    def vigenere(input_str, key_word):
         """ Vigenere encryption
             input str : string
             key_word : string
             language : "eng", "rus"
         """
-        
-        if language == "eng":
-            alphabet = alphabet_eng
-        elif language == "rus":
-            alphabet = alphabet_rus
+        alphabet = self.alphabet
 
         key_word = key_word.lower()
         alphabet_length = len(alphabet)
@@ -109,19 +108,15 @@ class Viginere(AbstractCipher):
         output_str = ''.join(new_str)
         return output_str
 
-    def vigenere_decryption(input_str, key_word, language = "eng"):
-        
-        if language == "eng":
-            alphabet = alphabet_eng
-        elif language == "rus":
-            alphabet = alphabet_rus
+    def vigenere_decryption(input_str, key_word):
+        alphabet = self.alphabet
         
         reversed_key_word = []
         key_word = key_word.lower()
         for letter in key_word:
             reversed_key_word.append(alphabet1[len(alphabet) - alphabet.index(letter)])
         reversed_key_word = ''.join(reversed_key_word)
-        return vigenere(input_str, reversed_key_word, language)
+        return vigenere(input_str, reversed_key_word)
 
 
 
@@ -157,32 +152,45 @@ class Vernam(AbstractCipher):
 
 class Frequency_analysis:
     def __init__(self, language):
-        self.language = language
+        if language == "eng":
+            self.most_commoly_used_letter_index = alphabet.index('e')
+            self.alphabet = alphabet_eng
+        elif language == "rus":
+            self.most_commoly_used_letter_index = alphabet.index('о')
+            self.alphabet = alphabet_rus
 
     def decrypt(self, input_str):
-        return frequency_analysis(input_str, self.language)
+        return frequency_analysis(input_str)
 
-    def frequency_analysis(input_str, language = "eng"):
+    def frequency_analysis(input_str):
         """ Frequency Caesar decryption algorithm based on most commoly used letter
             input_str = string to decrypt
             language = {"eng", "rus"}
         """
-        
-        if language == "eng":
-            alphabet = alphabet_eng
-            most_commoly_used_letter_index = alphabet.index('e')
-        elif language == "rus":
-            alphabet = alphabet_rus
-            most_commoly_used_letter_index = alphabet.index('о')
-        
+        alphabet = self.alphabet
+        most_commoly_used_letter_index = self.most_commoly_used_letter_index
+
         frequency_rate = [0] * len(alphabet)
         for letter in input_str.lower():
             if letter.isalpha():
                 frequency_rate[alphabet.index(letter)] += 1
                 
         most_common_letter_index = frequency_rate.index(max(frequency_rate))
-        bias = most_common_letter_index - most_commoly_used_letter_index
-        return caesar_decryption(input_str, bias, language)
+        bias = -(most_common_letter_index - most_commoly_used_letter_index)
+        """caesar(input_str, bias)"""
+        alphabet_length = len(alphabet)
+        new_str = []
+        for letter in input_str:
+            if letter in alphabet:
+                new_str.append(alphabet[(alphabet.index(letter) + bias) % alphabet_length])
+            elif letter in [i.upper() for i in alphabet]:
+                new_str.append(alphabet[(alphabet.index(letter.lower()) + bias) % alphabet_length].upper())
+            elif letter.isalpha():
+                raise TextError("Wrong alphabet")
+            else:
+                new_str.append(letter)
+        output_str = ''.join(new_str)
+        return output_str
 
 
 def main():
